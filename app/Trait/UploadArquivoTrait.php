b619a2eedbc3a6b020267d141ceebcf871243ef6
Flademir Morais <fladermorais@hotmail.com>
2021-07-18 11:20:02 -0300

    [Trait] Criando um Trait para tratar os Uploads

---
+++ app/Trait/UploadArquivoTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Traits;
+
+class UploadArquivoTrait {
+  
+  
+  public function uploadArquivo($arquivo, $alias, $tipo)
+  {
+    $file = $arquivo;
+    $extensao = $arquivo->getClientOriginalExtension();
+    $nomeArquivo =  $alias. "." .$extensao;
+    $path = public_path('/storage/'. $tipo .'/');
+    
+    $file->move($path, $nomeArquivo);
+    
+    if(!$file){
+      flash('Falha ao fazer o upload do Arquivo')->warning();
+      return redirect()
+      ->back()
+      ->with('error', 'Falha ao fazer upload do Arquivo')
+      ->withInput();
+    }
+    return $nomeArquivo;
+  }
+  
+  public function unlinkArquivo($arquivo, $tipo)
+  {
+    $path = public_path('/storage/'. $tipo . '/');
+    $file = $arquivo;
+    $arquivo = $path.$file;
+    if(file_exists($arquivo)){
+      unlink($arquivo);
+    }
+    
+    return true;
+  }
+  
+  function getAlias($string){
+    $string = str_replace("?", "_", $string);
+    $string = str_replace("!", "_", $string);
+    $string = str_replace(",", "_", $string);
+    $string = str_replace(' ', "_", $string);
+    return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/","/(ç)/","/(Ç)/"),explode(" ","a A e E i I o O u U n N c C"),$string);
+  }
+}
