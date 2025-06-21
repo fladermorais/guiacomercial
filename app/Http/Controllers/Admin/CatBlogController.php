951c242e3476c18189dc23387ff693be5c7186d0
Flademir Morais <fladermorais@hotmail.com>
2025-06-21 18:31:42 -0300

    [Categoria Blog] Adicionando gate no controller

--- app/Http/Controllers/Admin/CatBlogController.php
+++ app/Http/Controllers/Admin/CatBlogController.php
@@ -6,17 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoriaBlogRequest;
 use App\Models\CatBlog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class CatBlogController extends Controller
 {
     public function index()
     {
+        if(Gate::denies('categoriaBlog.index')){
+            abort(403, "Não Autorizado");
+        }
         $categorias = CatBlog::orderBy('status')->orderBy('titulo')->get();
         return view('Admin.categoriaBlog.index', compact('categorias'));
     }
     
     public function search(Request $request)
     {
+        if(Gate::denies('categoriaBlog.index')){
+            abort(403, "Não Autorizado");
+        }
         $data = $request->all();
         
         $categorias = CatBlog::where('titulo', 'like', '%' . $data['search']. '%')->get();
@@ -29,11 +36,17 @@ class CatBlogController extends Controller
     
     public function create()
     {
+        if(Gate::denies('categoriaBlog.create')){
+            abort(403, "Não Autorizado");
+        }
         return view('Admin.categoriaBlog.create');
     }
     
     public function store(CategoriaBlogRequest $request)
     {
+        if(Gate::denies('categoriaBlog.create')){
+            abort(403, "Não Autorizado");
+        }
         $data = $request->all();
         $categoria = new CatBlog;
         $response = $categoria->newInfo($data);
@@ -45,12 +58,18 @@ class CatBlogController extends Controller
     
     public function edit(CatBlog $categoriaBlog)
     {
+        if(Gate::denies('categoriaBlog.edit')){
+            abort(403, "Não Autorizado");
+        }
         $categoria = $categoriaBlog;
         return view('Admin.categoriaBlog.edit', compact('categoria'));
     }
     
     public function update(CategoriaBlogRequest $request, CatBlog $categoriaBlog)
     {
+        if(Gate::denies('categoriaBlog.edit')){
+            abort(403, "Não Autorizado");
+        }
         $data = $request->all();
         $response = $categoriaBlog->updateInfo($data);
         if($response){
