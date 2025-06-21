3bb20a6786734b38d73a07d9c3abaf6887496785
Flademir Morais <fladermorais@hotmail.com>
2025-06-21 18:25:00 -0300

    [Service] Criando um service para o uplaod de arquivos

---
+++ app/Services/UploadFileService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+class UploadFileService {
+  
+  
+  public static function uploadArquivo($arquivo, $alias, $tipo)
+  {
+    $file = $arquivo;
+    $extensao = $arquivo->getClientOriginalExtension();
+    $nomeArquivo =  $alias. "." .$extensao;
+    $path = public_path('/storage/'. $tipo .'/');
+    
+    $file->move($path, $nomeArquivo);
+    
+    if(!$file){
+      flash('Falha ao fazer o upload do Arquivo')->warning();
+      return redirect()
+      ->back()
+      ->with('error', 'Falha ao fazer upload do Arquivo')
+      ->withInput();
+    }
+    return $nomeArquivo;
+  }
+  
+  public static function unlinkArquivo($arquivo, $tipo)
+  {
+    $path = public_path('/storage/'. $tipo . '/');
+    $file = $arquivo;
+    $photo = $path.$file;
+
+    if (is_dir($photo)) {
+      // rmdir($photo); // remove diretório vazio
+    } elseif (is_file($photo)) {
+      unlink($photo); // remove arquivo
+    }
+    
+    return true;
+  }
+
+  public static function getAlias($string){
+    $string = str_replace("?", "_", $string);
+    $string = str_replace("!", "_", $string);
+    $string = str_replace(",", "_", $string);
+    $string = str_replace(' ', "_", $string);
+    return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/","/(ç)/","/(Ç)/"),explode(" ","a A e E i I o O u U n N c C"),$string);
+  }
+}
